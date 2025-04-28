696717dc9780a44f001b675053bc15abd985fa0a
salonifojdar27 <salonifojdar27@gmail.com>
2025-04-28 11:35:40 +0530

    Add files via upload

---
+++ Recipe js final exam/script.js
@@ -0,0 +1,18 @@
+
+const RecipeInput = document.getElementById("Recipe-input");
+const ingredients = document.getElementById("ingredients");
+const instructions = document.getElementById("instructions");
+const search = document.getElementById("search");
+const AddBtn = document.getElementById("AddBtn");
+
+function addingRecipe(){
+   
+}
+
+function deleteRecipe(){
+
+}
+
+function EditRecipe(){
+    
+}
